b427764b8c9dc96b59921d9d80af446ceb077d91
joaovclisboa <sp3ctros@hotmail.com>
2025-01-07 02:16:04 -0300

    base para o hpp

--- jogos.hpp
+++ jogos.hpp
@@ -1,5 +1,15 @@
 #ifndef jogos_hpp
 #define jogos_hpp
+#include <iostream>
+#include <vector>
 
-
+class JogosDeTabuleiro{
+    public:
+        std::vector<std::vector<char>> Tabuleiro;
+        int getLinhas() const;
+        int getColunas() const;
+        int setLinhasColunas(int linhas, int colunas);
+    private:
+        int linhas, colunas;
+};
 #endif
