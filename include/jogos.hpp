10377e40de0cad780b3dc986fc6804289765d8ae
Capelão <65136875+Joed-skec@users.noreply.github.com>
2025-01-20 09:06:09 -0300

    Update jogos.hpp

--- include/jogos.hpp
+++ include/jogos.hpp
@@ -49,5 +49,14 @@ public:
     bool verificar_jogada(int linha, int coluna, int jogador) const override;
     bool testar_condicao_de_vitoria() const override;
 };
+class Lig4 : public JogosDeTabuleiro{
+public:
+
+    Lig4(int linhas = 6, int colunas = 7);
+
+    int ler_jogada(int coluna, int jogador) override;
+    bool verificar_jogada(int coluna, int jogador) const override;
+    bool testar_condicao_de_vitoria() const override;
+};
 
 #endif
